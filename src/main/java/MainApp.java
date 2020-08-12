15d5b1f187e2a7e0238bd06280698bcd4f29a0a7
Your Name <you@example.com>
2020-08-12 15:58:19 +0700

    Изм 7

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -3,11 +3,22 @@ import java.util.*;
 public class MainApp {
     //сумма типа byte
     public static byte byteSum(byte b, short s) {
-        return (byte) (b+s);
+        short sum = (short) (b + s);
+        if (sum >= Byte.MAX_VALUE) {
+            return Byte.MAX_VALUE;
+        } else {
+            return (byte) sum;
+
+        }
     }
     //произведение типа int
     public static int intMult(int i, long l) {
-        return (int) (i*l);
+        long mul = i*l;
+        if (mul >= Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        } else {
+            return (int) (i * l);
+        }
     }
     //максимум из двух чисел типа int
     public static int getMaxNumber (int a, int b) {
@@ -28,7 +39,7 @@ public class MainApp {
         }
     }
     //проверка на принадлежность к числу
-    public boolean isCharNumber(char c) {
+    public static boolean isCharNumber(char c) {
         return Character.isDigit(c);
     }
     //вывод бинарного представления 0-30
@@ -40,16 +51,30 @@ public class MainApp {
     }
     //вывод бинарного представления Integer.MAX_VALUE, MAX_VALUE+1
     public static void binaryIntMaxVal() {
-        long intMaxValPlusOne = Integer.MAX_VALUE + 1;
-        System.out.println("Integer.MAX_VALUE =" + Integer.MAX_VALUE + "-->" + Integer.toBinaryString(Integer.MAX_VALUE));
-        System.out.println("Integer.MAX_VALUE+1 =" + (Integer.MAX_VALUE + 1) + "-->" + Integer.toBinaryString(Integer.MAX_VALUE+1));
+        //long intMaxValPlusOne = Integer.MAX_VALUE + 1;
+        System.out.println("Integer.MAX_VALUE =" + Integer.MAX_VALUE + "------>" + Integer.toBinaryString(Integer.MAX_VALUE));
+        System.out.println("Integer.MAX_VALUE+1 =" + (Integer.MAX_VALUE + 1) + "-->" + Integer.toBinaryString(Integer.MAX_VALUE + 1));
     }
-
+        //char в число
+    public static int charTiNumber(char c) {
+        return (int) c;
+    }
+    //число в char
+    public static char intToChar(int i) {
+        return Character.toChars(i)[0] ;
+    }
         public static void main(String[] args) {
+        System.out.println(byteSum((byte) 3, (short) 120));
+        System.out.println(intMult(5, 1474836480));
+        System.out.println(getMaxNumber(4,8));
+        System.out.println(isCharA('v'));
+        System.out.println(isCharNumber('8'));
         binaryShow();
         binaryIntMaxVal();
-        System.out.println(78217821);
+        System.out.println(charTiNumber('c'));
+        System.out.println(intToChar(99));
 
-    }
+
+        }
 }
 
